d83bfe4b9b0ea11a804cbe32e5b857f681f86d43
Daniel Pastor <89154301+PastorPass@users.noreply.github.com>
2022-08-08 17:54:48 -0300

    Add files via upload

---
+++ Constantes.java
@@ -0,0 +1,13 @@
+
+public class Main {
+    public static void main(String[] args) {
+        char caracter = 'a';
+        final int numero = 10;  // Para declarar constantes es al inicio declarar "final" (tipo de dato) 
+        System.out.println(numero);
+        System.out.println("el decimal es : " +caracter);
+        
+    }
+}
+
+
+// Las constantes pueden ser utilizadas con cualquier tipo de dato
