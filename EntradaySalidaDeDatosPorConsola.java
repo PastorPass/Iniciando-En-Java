bf062e3fe8b7e3d9d20ab0048107543e63257e50
Daniel Pastor <89154301+PastorPass@users.noreply.github.com>
2022-08-08 19:32:33 -0300

    Add files via upload

---
+++ EntradaySalidaDeDatosPorConsola.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner (System.in);
+        int numero;
+        
+        System.out.print("Digite un numero : ");
+        numero = entrada.nextInt();
+        
+        System.out.println("El numero es " + numero);
+        
+    }
+}
